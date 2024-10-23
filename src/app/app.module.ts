afcef965d6a3148d82d60d1cd29083d759634a30
Alexey <14volvox88@gmail.com>
2024-10-23 23:29:35 +0300

    Изначально начал проект в другой папке, потом просто всё сюда перекинул и заменил то, что совпадало

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,21 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WelcomeMenuComponent } from './welcome-menu/welcome-menu.component';
+import { RegistrationComponent } from './registration/registration.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeMenuComponent,
+    RegistrationComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
